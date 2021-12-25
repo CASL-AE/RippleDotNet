777dca2482029555da5888817fd6d2c30ac1a45b
Denis Angell <denis@harpangell.com>
2021-12-25 03:08:06 -0500

    nft feature

---
+++ src/RippleDotNet/Responses/Transaction/TransactionTypes/NFTokenBurnTransactionResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+using RippleDotNet.Json.Converters;
+using RippleDotNet.Model;
+using RippleDotNet.Model.Transaction.Interfaces;
+using RippleDotNet.Responses.Transaction.Interfaces;
+
+namespace RippleDotNet.Responses.Transaction.TransactionTypes
+{
+    public class NFTokenMintTransactionResponse : TransactionResponseCommon, INFTokenMintTransaction
+    {
+        [JsonConverter(typeof(RippleDateTimeConverter))]
+        public DateTime? Expiration { get; set; }
+
+        public NFTokenMintFlags? Flags { get; set; }
+
+        public uint? TokenTaxon { get; set; }
+
+        public string Issuer { get; set; }
+
+        public uint? TransferFee { get; set; }
+
+        public string URI { get; set; }
+    }
+}
